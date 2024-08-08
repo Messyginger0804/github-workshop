443597149dff97e48d4b276d1bd13e756e65aab4
JC <j.c.ashley4363@gmail.com>
2024-08-08 08:45:08 -0500

    getting wesite files back

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js (Main Branch)
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.getElementById("header");
+    header.textContent = "I love working at iServiceWeb";
+
+    const changeImageBtn = document.getElementById("changeImageBtn");
+    const picture = document.getElementById("picture");
+
+    const image1 = "assets/hey.png";
+    const image2 = "assets/new_picture.png";
+
+    changeImageBtn.addEventListener("click", function () {
+        if (picture.src.includes(image1)) {
+            picture.src = image2;
+            picture.alt = "New Picture";
+        } else {
+            picture.src = image1;
+            picture.alt = "Your Picture";
+        }
+    });
+});
